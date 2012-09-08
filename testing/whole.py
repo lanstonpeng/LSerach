f71b297ef3a4ec6a64d073957948e64dd077875c
lanstonpeng <lanstonpeng@gmail.com>
2012-09-08 10:06:02 +0800

    check

--- testing/whole.py
+++ testing/whole.py
@@ -104,3 +104,23 @@ def crawl_web(seed,max_pages):
             union(tocrawl,outlinks)
             crawled.append(page)
     return index,graph
+
+def compute_ranks(graph):
+    d = 0.8 # damping factor
+    numloops = 10
+
+    ranks = {}
+    npages = len(graph)
+    for page in graph:
+        ranks[page] = 1.0 / npages
+
+    for i in range(0, numloops):
+        newranks = {}
+        for page in graph:
+            newrank = (1 - d) / npages
+            for node in graph:
+                if page in graph[node]:
+                    newrank += d*rank[node]/len(graph[node])
+            newranks[page] = newrank
+        ranks = newranks
+    return ranks
